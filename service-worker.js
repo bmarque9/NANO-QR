62c1a2cb87916845a58e177d7c495dd0be91af4c
bmarque9 <bryanmargj@icloud.com>
2026-09-01 13:48:10 -0600

    Change cache name and enhance fetch event logic
    Updated cache name and improved fetch handling.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nano-qr-v4";
+const CACHE_NAME = "nano-qr-v11";
 
 const ARCHIVOS = [
   "./",
@@ -22,22 +22,48 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cached => {
+
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request).then(response => {
+
+        const copia =
+          response.clone();
+
+        caches
+          .open(CACHE_NAME)
+          .then(cache => {
+            cache.put(
+              event.request,
+              copia
+            );
+          });
+
+        return response;
+
+      });
+
     })
   );
 });
